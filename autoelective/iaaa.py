0ff6acb7978164a82c8ba000867e05ea447047e2
jy5275 <527507046@qq.com>
2020-05-30 15:50:05 +0800

    mod: send cookie in oauth_login

--- autoelective/iaaa.py
+++ autoelective/iaaa.py
@@ -4,6 +4,7 @@
 # modified: 2019-09-10
 
 import random
+import requests
 from urllib.parse import quote
 from .client import BaseClient
 from .hook import get_hooks, debug_print_request, check_status_code, check_iaaa_success
@@ -17,6 +18,7 @@ _hooks_check_iaaa_success = get_hooks(
 
 
 class IAAAClient(BaseClient):
+    iaaa_cookie = ""
 
     default_headers = {
         "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -31,11 +33,32 @@ class IAAAClient(BaseClient):
         "Sec-Fetch-Site": "same-origin",
     }
 
+    def get_cookie(self):
+        self.default_headers["Referer"] = "https://elective.pku.edu.cn/elective2008/"
+        self.default_headers["Upgrade-Insecure-Requests"] = "1"
+        r = requests.get(
+            url="https://iaaa.pku.edu.cn/iaaa/oauth.jsp?appID=syllabus&appName=%E5%AD%A6%E7%94%9F%E9%80%89%E8%AF%BE%E7%B3%BB%E7%BB%9F&redirectUrl=http://elective.pku.edu.cn:80/elective2008/ssoLogin.do",
+            params={
+                "appid": "syllabus",
+                "appName": "",
+                "redirectUrl": "http://elective.pku.edu.cn:80/elective2008/ssoLogin.do"
+            },
+            headers=self.default_headers
+        )
+        if isinstance(r.headers.get('Set-Cookie'), str) == True:
+            cookiestr = r.headers['Set-Cookie']
+            beg = cookiestr.find("=", 0)+1
+            end = cookiestr.find(";", 0)
+            self.iaaa_cookie = cookiestr[0:end]
+
     def oauth_login(self, username, password, **kwargs):
+        self.get_cookie()
+
         headers = kwargs.pop("headers", {})
-        headers["Referer"] = "%s?appID=syllabus&appName=%s&redirectUrl=%s" % (
+        self.default_headers["Referer"] = "%s?appID=syllabus&appName=%s&redirectUrl=%s" % (
             IAAAURL.OauthHomePage, quote("学生选课系统"), ElectiveURL.SSOLoginRedirect,
         )
+        self.default_headers["Cookie"] = self.iaaa_cookie
         r = self._post(
             url=IAAAURL.OauthLogin,
             data={
@@ -47,7 +70,7 @@ class IAAAClient(BaseClient):
                 "otpCode": "",
                 "redirUrl": ElectiveURL.SSOLoginRedirect,
             },
-            headers=headers,
+            headers=self.default_headers,
             hooks=_hooks_check_iaaa_success,
             **kwargs,
         )
